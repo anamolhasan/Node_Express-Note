5f89e3f8667a5ba4032f60b8ae4ba836164bbc9f
anamolhasan <mdanamolhasan1998@gmail.com>
2025-12-08 15:29:12 +0600

    class 02

--- server-with-express/src/server.ts
+++ server-with-express/src/server.ts
@@ -1,6 +1,18 @@
 import express, { Request, Response } from 'express'
+import {Pool} from 'pg'
+
 const app = express()
 const port = 5000
+
+const pool = new Pool({
+    connectionString:`postgresql://neondb_owner:npg_adOElLe72pbP@ep-royal-mouse-ah9mynif-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`
+})
+
+
+
+// parser
+app.use(express.json())
+// app.use(express.urlencoded())
 
 app.get('/', (req : Request, res : Response) => {
   res.send('Hello World! Bangladesh')
